b1236e2082aef8c5ad4914bd9c4e1e2f30b2f0eb
soumik <soumikm.official@gmail.com>
2023-07-22 14:06:40 +0530

    hero recovered

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
-    <main className="">
-      <Image
-        src={
-          "https://pinnacle.works/wp-content/uploads/2022/06/dummy-image.jpg"
-        }
-        width={100}
-        height={100}
-        alt=""
-      />
-=======
     <main className="px-20 py-10">
       {/* hero section */}
       <div className="flex items-center flex-col gap-5 px-10">
@@ -26,7 +15,6 @@ export default function Home() {
           worked.
         </em>
       </div>
->>>>>>> 0c82d42ef7466a70a00f3189ffb346e10db960a4
     </main>
   );
 }
